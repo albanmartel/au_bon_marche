36225548676fcbc556617870a95a3445264b5637
Alban MARTEL <alban.martel@fms-ea.com>
2026-07-22 13:42:13 +0200

    Ajout de la méthode du constructeur et test basique instansiation objet de la classe

--- Customer.py
+++ Customer.py
@@ -4,15 +4,22 @@ from multiprocessing import process
 from os import name
 from typing import ClassVar
 from dataclasses import dataclass
+from datetime import datetime
 
 # Pour générer un id
 import uuid
 
 @dataclass
 class Customer:
-    pass
+
+    def __init__(self, surname, first_name, creation_date):
+        self.id = str(uuid.uuid4().hex)
+        self.surname = surname
+        self.first_name = first_name
+        self.creation_date = creation_date
 
 
 
 if __name__ == "__main__":
     print("Test de la classe Customer")
+    Customer("Martin", "Arthur", datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
