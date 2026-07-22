9a68f89655a44248ee5dc36563e13cb7729a1a68
Alban MARTEL <alban.martel@fms-ea.com>
2026-07-22 13:31:39 +0200

    Ajout d'un fichier de classe Customer(Client) et fonction main pour tester la classe

---
+++ Customer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#-*-coding: utf-8 -*/-
+from multiprocessing import process
+from os import name
+from typing import ClassVar
+from dataclasses import dataclass
+
+# Pour générer un id
+import uuid
+
+@dataclass
+class Customer:
+    pass
+
+
+
+if __name__ == "__main__":
+    print("Test de la classe Customer")
