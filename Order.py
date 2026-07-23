512bb2a5720919d4d12c76eaf163b09999cc095d
Philippe P <philippe.peyrous@fms-ea.com>
2026-07-23 11:37:25 +0200

    modif de f_delete_one_product

--- Order.py
+++ Order.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
 #-*-coding: utf-8 -*/-
 from datetime import datetime
-from typing import ClassVar
-from dataclasses import dataclass
 
-@dataclass
 class Order:
 
-    def __init__(self, id_order: str, quantity_products: int, order_date: datetime) ->None:
+    def __init__(self, id_order: str, order_date: datetime) ->None:
         self.id_order = id_order
+        # liste de couples (id_product, quantité) car la quantité appartient à chaque ligne de commande
         self.products = []
-        self.quantity_products_ask = quantity_products
         self.order_date = order_date
 
-
-    @classmethod
-    def f_add_product(self, product):
-        self.products.append(product)
+    def f_add_product(self, product_id: int, quantity: int):
+        self.products.append((product_id, quantity))
 
 
-    @classmethod
-    def f_delete_product(self, product):
-        self._products.remove(product)
+    # suppression de tous les produit concernés, sur toutes les ligne de la commande, sans se preoccuper de la quantité demandée.
+    # je reconstruit une liste sans les produits concernés
+    # à activer si un client commande plus que le stock ou se trompe
+    def f_delete_one_product(self, product_id: int):
+        self.products = [
+            (id_product, quantity)
+            for id_product, quantity in self.products
+            if id_product != product_id
+        ]
+
+
