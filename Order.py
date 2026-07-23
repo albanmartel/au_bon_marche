527147ad442c1aaaccdf5e20c86e1144a312d4bf
Philippe P <philippe.peyrous@fms-ea.com>
2026-07-23 14:09:21 +0200

    modif de init pour dico de pdt et du add et delete

--- Order.py
+++ Order.py
@@ -1,42 +1,25 @@
 #!/usr/bin/env python3
 #-*-coding: utf-8 -*/-
 from datetime import datetime
-import Product
-
-<<<<<<< HEAD
 
 class Order:
 
     def __init__(self, id_order: str, order_date: datetime) ->None:
         self.id_order = id_order
-        # liste de couples (id_product, quantité) car la quantité appartient à chaque ligne de commande
-        self.products = []
+        # dico (id_product, quantité) car la quantité appartient à chaque produit
+        self.products = {}
         self.order_date = order_date
 
+
     def f_add_product(self, product_id: int, quantity: int):
-        self.products.append((product_id, quantity))
-
-    # suppression de tous les produit concernés, sur toutes les ligne de la commande, sans se preoccuper de la quantité demandée.
-    # je reconstruit une liste sans les produits concernés
-    # à activer si un client commande plus que le stock ou se trompe
-    def f_delete_one_product(self, product_id: int):
-        self.products = [
-            (id_product, quantity)
-            for id_product, quantity in self.products
-            if id_product != product_id
-        ]
-
-    # suppression de tous les produit concernés, sur toutes les ligne de la commande, sans se preoccuper de la quantité demandée.
-    # je reconstruit une liste sans les produits concernés
-    # à activer si un client commande plus que le stock ou se trompe
+        if product_id in self.products:
+            self.products[product_id] += quantity
+        else:
+            self.products[product_id] = quantity
 
     def f_delete_one_product(self, product_id: int):
-        self.products = [
-            (id_product, quantity)
-            for id_product, quantity in self.products
-            if id_product != product_id
-        ]
-
+        if product_id in self.products:
+            del self.products[product_id]
 
     def __str__(self):
         txt_products = ""
