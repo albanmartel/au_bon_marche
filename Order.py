3dbd26e9c272159ca2cf444ecc60a68ed0b83b65
Alban MARTEL <alban.martel@fms-ea.com>
2026-07-23 16:14:07 +0200

    Création d'une nouvelle méthode pour ajouter unproduit avec sa quantité et son prix à une commande pour parvenir à calculer les prix de la commande dans le class Oder

--- Order.py
+++ Order.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #-*-coding: utf-8 -*/-
 from datetime import datetime
+import Product
 
 class Order:
 
@@ -17,6 +18,16 @@ class Order:
         else:
             self.products[product_id] = quantity
 
+
+    def f_add_product_quantity_price(self, product_id: int, quantity: int, price: float):
+        if product_id in self.products:
+            old_quantity = self.products[product_id][0]
+            new_quantity = int(old_quantity) + quantity
+            self.products[product_id] += str(new_quantity), str(price)
+        else:
+            self.products[product_id] = str(quantity), str(price)
+
+
     def f_delete_one_product(self, product_id: int):
         if product_id in self.products:
             del self.products[product_id]
@@ -24,15 +35,24 @@ class Order:
 
     def __str__(self):
         txt_products = ""
+        print(self.products)
         for product in self.products:
-            txt_products += product.__str__() + " "
+            tuple_product = self.products[product]
+            txt_products += f"Id_product: {str(product)}\nQuantité: {str(tuple_product[0])}\nPrix/unité:{str(tuple_product[1])} €\n"
 
         return f"Commande n°{self.id_order}\nLe(s) produit(s): {txt_products}\nCommande créé le: {self.order_date}"
+    
 
 
 if __name__ == "__main__":
+    print("Test la Classe Order")
     order = Order("01", datetime.today())
     #product = Product(1, "Mandarine", "Fruit", 6, 2.8, "kg")
-    order.f_add_product(1, 5)
+    #order.f_add_product(1, 5)
+    # Mandarine
+    order.f_add_product_quantity_price(1,5, 2.8)
+    # Epinard
+    order.f_add_product_quantity_price(2, 4, 2.8)
+
     print(order)
 
