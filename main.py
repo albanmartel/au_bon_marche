da5b5a7d5823d0696a4a292aa4e2423eb42fcaab
Alban MARTEL <alban.martel@fms-ea.com>
2026-07-24 08:50:53 +0200

    Ajout d'un fichier main.py un fichier comme l'anneau du Seigneur des anneaux pour les diriger tous ses objets

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#-*-coding: utf-8 -*/-
+
+"""
+Ce fichier est le fichier de lancement de l'applications
+" Au BON MARCHE"
+Il permet d'utiliser toutes les classes créés
+dans le répertoire du projet
+@projet de l'académie FMS Python JAVA
+Philippe PEYROUS
+Alban MARTEL
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
